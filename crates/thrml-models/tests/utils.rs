37c264a1a597cd68e275fbc7915958760a630fc4
SashimiSaketoro <97864728+SashimiSaketoro@users.noreply.github.com>
2025-11-27 13:55:48 -0600

    fix: handle Spherical node type in test match

--- crates/thrml-models/tests/utils.rs
+++ crates/thrml-models/tests/utils.rs
@@ -226,8 +226,10 @@ pub fn sample_and_compare_distribution<E: AbstractEBM>(
                 NodeType::Spin => all_binary_nodes.push(node.clone()),
                 NodeType::Categorical { .. } => all_cat_nodes.push(node.clone()),
                 NodeType::Continuous => {
-                    // Continuous nodes are not supported in discrete EBM tests
                     panic!("Continuous nodes not supported in discrete EBM tests")
+                }
+                NodeType::Spherical { .. } => {
+                    panic!("Spherical nodes not supported in discrete EBM tests")
                 }
             }
         }
